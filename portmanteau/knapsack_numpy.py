d867507a0647179d3e7aa04acfbe305482c2a2c9
藤平燎 <always.fujihira@gmail.com>
2019-11-18 01:05:59 +0900

    ABC145E

---
+++ portmanteau/knapsack_numpy.py
@@ -0,0 +1,17 @@
+"""
+N M
+V[0] W[0]
+...
+V[N-1] W[N-1]
+という入力が与えられたとき, 
+Wの合計がMを超えないようにVの合計を最大化する.
+"""
+
+import numpy as np
+N, M = map(int, input().split())
+VW = [tuple(map(int, input().split())) for n in range(N)]
+VW.sort(key=lambda x: x[1])
+DP = np.zeros(M+1, dtype=int)
+for v, w in VW:
+    np.maximum(DP[:-w]+v, DP[w:], out=DP[w:])
+print(DP[M])
