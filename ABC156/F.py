5cd76bcb6f1009703c92745723fab5214e88b592
fujihiraryo <fujihiraryo@team-lab.com>
2020-02-23 04:55:57 +0900

    ABC156

--- ABC156/F.py
+++ ABC156/F.py
@@ -2,12 +2,23 @@ K, Q = map(int, input().split())
 *D, = map(int, input().split())
 for q in range(Q):
     n, x, m = map(int, input().split())
-    D1 = [d % m for d in D]
     r = (n - 1) % K
-    cnt_0 = D1.count(0) * ((n - 1) // K) + D1[:r].count(0)
+    D1r_count0 = 0
+    D1_count0 = 0
+    D1_sum = 0
+    D1r_sum = 0
+    for k in range(K):
+        if k < r:
+            if D[k] % m == 0:
+                D1r_count0 += 1
+            D1r_sum += D[k] % m
+        if D[k] % m == 0:
+            D1_count0 += 1
+        D1_sum += D[k] % m
+    cnt_0 = D1_count0 * ((n - 1) // K) + D1r_count0
     a = x
     # 第n-1項を求める
-    b = a + sum(D1) * ((n - 1) // K) + sum(D1[:r])
+    b = a + D1_sum * ((n - 1) // K) + D1r_sum
     # 繰り上がりの回数
     cnt_p = b // m - a // m
     cnt_n = (n - 1) - cnt_0 - cnt_p
