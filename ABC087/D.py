0eadca86beae07adb2703098f83a0c7d08b81d85
藤平燎 <fujihiraryo@team-lab.com>
2020-04-08 14:38:27 +0900

    リファクタリング

--- ABC087/D.py
+++ ABC087/D.py
@@ -4,9 +4,8 @@ inf = 10**10
 G = [{} for i in range(n)]
 for i in range(m):
     l, r, d = map(int, input().split())
-    if l > r:
-        l, r, d = r, l, -d
     G[l-1][r-1] = d
+    G[r-1][l-1] = -d
 # D[j] = jの属する連結成分のトップからjへの距離
 D = [inf for i in range(n)]
 # まだ探索していないiをトップとして探索する
@@ -20,6 +19,7 @@ for i in range(n):
         for k in G[j].keys():
             if D[k] == inf:
                 Q.append(k)
+            if D[k] == inf:
                 D[k] = D[j]+G[j][k]
             elif D[k] != D[j]+G[j][k]:
                 print('No')
