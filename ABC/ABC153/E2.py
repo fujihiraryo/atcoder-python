ca3103fc39aae2a5fd7f3ecda65be13b658825c5
藤平燎 <fujihiraryo@team-lab.com>
2020-04-19 06:42:59 +0900

    ABC153

---
+++ ABC/ABC153/E2.py
@@ -0,0 +1,12 @@
+h, n = map(int, input().split())
+AB = [tuple(map(int, input().split())) for i in range(n)]
+A = [a for a, b in AB]
+B = [b for a, b in AB]
+inf = 10**10
+# DP[i][j]=i個以内で体力j以上削るための消費魔力の最小値
+DP = [[inf for j in range(h+1)] for i in range(n+1)]
+DP[0][0] = 0
+for i in range(n):
+    for j in range(h+1):
+        DP[i+1][j] = min(DP[i][j], DP[i+1][max(0, j-A[i])]+B[i])
+print(DP[n][h])
