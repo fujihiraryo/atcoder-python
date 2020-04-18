52903d277baac1f854a0adb1fab75b97b7cc900e
藤平燎 <fujihiraryo@team-lab.com>
2020-04-19 04:22:40 +0900

    ABC153復習

--- ABC/ABC153/F.py
+++ ABC/ABC153/F.py
@@ -1,5 +1,21 @@
-N, D, A = map(int, input().split())
-X = []
-for n in range(N):
+n, d, a = map(int, input().split())
+XH = []
+for i in range(n):
     x, h = map(int, input().split())
-    X.append((x, h))
+    XH.append((x, h))
+XH.sort()
+X = [x for x, h in XH]
+H = [h for x, h in XH]
+X.append(10**10)
+H.append(0)
+j = 0
+c = 0
+S = [0]*(n+1)
+for i in range(n):
+    H[i] = max(0, H[i]-S[i])
+    c += -(-H[i]//a)
+    while X[j] <= X[i] + 2*d:
+        j += 1
+    S[i+1] += S[i] + -(-H[i]//a)*a
+    S[j] -= -(-H[i]//a)*a
+print(c)
