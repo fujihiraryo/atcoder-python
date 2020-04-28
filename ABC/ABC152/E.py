21d240b654e7f5895310e1deb765da5556fd8e61
藤平燎 <fujihiraryo@team-lab.com>
2020-04-29 04:04:57 +0900

    復習

--- ABC/ABC152/E.py
+++ ABC/ABC152/E.py
@@ -1,8 +1,10 @@
 from functools import reduce
 import fractions
-N = int(input())
-* A, = map(int, input().split())
-mod = 10**9+7
+# input()
+# *A, = map(int, input().split())
+A = [10**6-i for i in range(10**4)]
+# A = [1000000, 999999, 999998]
+p = 10**9+7
 
 
 def gcd(a, b):
@@ -10,12 +12,9 @@ def gcd(a, b):
 
 
 def lcm(a, b):
-    return a*b//gcd(a, b)
+    return a*b*pow(gcd(a, b), p-2, p)
 
 
-LCM = reduce(lcm, A)
-
-ans = 0
-for a in A:
-    ans += LCM * pow(a, mod - 2, mod) % mod
-print(ans % mod)
+l = reduce(lcm, A) % p
+print(l)
+print(sum([l*pow(a, p-2, p) for a in A]) % p)
