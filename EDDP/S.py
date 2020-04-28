63bc6de3b675c1cf05cc270b41982a9b5405bf06
藤平燎 <fujihiraryo@team-lab.com>
2020-04-28 10:55:55 +0900

    桁DPやりなおし

--- EDDP/S.py
+++ EDDP/S.py
@@ -1,25 +1,16 @@
-n = int(input())
+N = [int(n) for n in list(input())]
 d = int(input())
 p = 10**9+7
-if d == 1:
-    print(n % p)
-    exit()
-N = [int(i) for i in list(str(n))]
-DP0 = [0] * d
-DP1 = [0] * d
-DP1[0] += 1
-for i in N:
-    DP0_ = [0] * d
-    DP1_ = [0] * d
-    for r in range(d):
-        for j in range(i):
-            DP0_[r] += DP1[(r-j) % d]
-            DP1_[r] += DP1[(r-j) % d]
-        DP0_[r] += DP0[(r-i) % d]
-        DP1_[r] += DP1[(r-i) % d]
-        for j in range(i+1, 10):
-            DP0_[r] += DP0[(r-j) % d]
-            DP1_[r] += DP0[(r-j) % d]
-        DP0_[r], DP1_[r] = DP0_[r] % p, DP1_[r] % p
-    DP0, DP1 = DP0_, DP1_
-print((DP1[0]-1) % p)
+DP = [[0]*d, [0]*d]
+DP[1][0] = 1
+for n in N:
+    DP_ = [[0]*d, [0]*d]
+    for i in range(2):
+        for j in range(d):
+            for k in range(n+i):
+                DP_[i][j] += DP[1][(j-k) % d] % p
+            for k in range(n+i, 10):
+                DP_[i][j] += DP[0][(j-k) % d] % p
+            DP_[i][j] %= p
+    DP = DP_
+print((DP[1][0] + p - 1) % p)
--- EDDP/S1.py
+++ EDDP/S1.py
@@ -1,6 +1,31 @@
-n = int(input())
-d = int(input())
+import sys
+sys.setrecursionlimit(100000)
+# n = int(input())
+# d = int(input())
+n = 10**10000
+d = 100
 p = 10**9+7
 DP = {}
-for i in range(10):
-    DP[i] = [j % d for j in range(d)]
+DP[-1] = [0]*d
+DP[0] = [0]*d
+DP[0][0] = 1
+
+
+def dp(i):
+    try:
+        return DP[i]
+    except:
+        pass
+    q, r = i//10, i % 10
+    DP[i] = []
+    for j in range(d):
+        tmp = 0
+        for k in range(r+1):
+            tmp += dp(q)[(j-k) % d]
+        for k in range(r+1, 10):
+            tmp += dp(q-1)[(j-k) % d]
+        DP[i].append(tmp % p)
+    return DP[i]
+
+
+print((dp(n)[0]-1) % p)
