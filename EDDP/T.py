60372a324d2269d387b43a2b6dca065b8a187536
藤平燎 <fujihiraryo@team-lab.com>
2020-04-29 21:59:57 +0900

    T - Permutation

---
+++ EDDP/T.py
@@ -0,0 +1,15 @@
+n = int(input())
+S = input()
+p = 10**9+7
+DP = [[0 for j in range(n+1)] for i in range(n+1)]
+DP[0][n] = 0
+for i in range(2, n+1):
+    A = [0]
+    for j in range(n):
+        A.append(A[-1]+DP[i-1][j])
+    for j in range(n-i+1):
+        if S[i-2] == '<':
+            DP[i][j] = (A[n-(i-1)+1]-A[j+1]) % p
+        else:
+            DP[i][j] = A[j+1] % p
+print(DP[n][0])
