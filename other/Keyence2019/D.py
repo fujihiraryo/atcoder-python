14fa8772e66b6ef5286d4ed39658855c1a938551
藤平燎 <fujihiraryo@team-lab.com>
2020-05-10 19:24:24 +0900

    move

--- other/Keyence2019/D.py
+++ other/Keyence2019/D.py
@@ -12,12 +12,12 @@ if len(setA) < n or len(setB) < m:
 mod = 10 ** 9 + 7
 cnt = 1
 for i in range(n * m, 0, -1):
-    cntA = n-bisect.bisect_left(A, i)
-    cntB = m-bisect.bisect_left(B, i)
+    cntA = n - bisect.bisect_left(A, i)
+    cntB = m - bisect.bisect_left(B, i)
     if i in setA and i not in setB:
-        cnt = (cnt*cntB) % mod
+        cnt = (cnt * cntB) % mod
     if i not in setA and i in setB:
-        cnt = (cnt*cntA) % mod
+        cnt = (cnt * cntA) % mod
     if i not in setA and i not in setB:
-        cnt = (cnt*(cntA*cntB-(n*m-i))) % mod
+        cnt = (cnt * (cntA * cntB - (n * m - i))) % mod
 print(cnt)
