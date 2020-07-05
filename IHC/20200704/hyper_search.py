dcce11c29c3c32b9682e430488428a44d12349f9
藤平燎 <fujihiraryo@team-lab.com>
2020-07-05 20:57:47 +0900

    ハイパラ探索

--- IHC/20200704/hyper_search.py
+++ IHC/20200704/hyper_search.py
@@ -10,17 +10,18 @@ def experiment(C, S, temp, threp, interval):
 
 
 patterns = []
-for temp in range(200, 1000, 200):
-    for threp in (0.2, 0.4, 0.6, 0.8):
-        for interval in range(1, 26 + 1, 5):
+for temp in (600, 1000, 2000):
+    for threp in (0.6, 0.8):
+        for interval in (1, 26, 52):
             patterns.append((temp, threp, interval))
 
-for ex in range(50):
-    scores = [0] * len(patterns)
+for ex in range(3):
+    scores = []
     C = [choice(range(100)) for j in range(26)]
     S = [[choice(range(20000)) for j in range(26)] for i in range(365)]
     for i, (temp, threp, interval) in enumerate(patterns):
-        scores[i] += experiment(C, S, temp, threp, interval)
-
-for pattern, score in zip(patterns, scores):
-    print(pattern, score)
+        scores.append(
+            ((temp, threp, interval), experiment(
+                C, S, temp, threp, interval)))
+    scores.sort(key=lambda _: _[1], reverse=True)
+    print(*scores[:5], sep="\n")
