ca1d316fb5e26dfaaa1fd87bfb83a1853312ea33
藤平燎 <fujihiraryo@team-lab.com>
2020-08-08 15:09:20 +0900

    tmp

--- other/MSOL2020/E.py
+++ other/MSOL2020/E.py
@@ -1,20 +1,32 @@
-from itertools import product
 n = int(input())
-XYP = [tuple(map(int, input().split())) for i in range(n)]
-X = [x for x, y, p in XYP]
-Y = [y for x, y, p in XYP]
-P = [p for x, y, p in XYP]
-A = [1 << 30] * (n + 1)
-for s in product(range(3), repeat=n):
-    c = s.count(1) + s.count(2)
-    d = 0
+X, Y, P = [], [], []
+for i in range(n):
+    x, y, p = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+    P.append(p)
+
+DX = [[0] * n for s in range(1 << n)]
+DY = [[0] * n for s in range(1 << n)]
+for s in range(1 << n):
     for i in range(n):
-        di = min(abs(X[i]), abs(Y[i])) * P[i]
+        DX[s][i] = abs(X[i]) * P[i]
+        DY[s][i] = abs(Y[i]) * P[i]
         for j in range(n):
-            if s[j] == 1:
-                di = min(di, abs(X[i] - X[j]) * P[i])
-            if s[j] == 2:
-                di = min(di, abs(Y[i] - Y[j]) * P[i])
-        d += di
-    A[c] = min(A[c], d)
+            if s & (1 << j):
+                DX[s][i] = min(DX[s][i], abs(X[i] - X[j]) * P[i])
+                DY[s][i] = min(DY[s][i], abs(Y[i] - Y[j]) * P[i])
+
+A = [10**20] * (n + 1)
+for sx in range(1 << n):
+    rx = ((1 << n) - 1) ^ sx
+    sy = rx
+    while sy >= 0:
+        sy &= rx
+        d = 0
+        for i in range(n):
+            d += min(DX[sx][i], DY[sy][i])
+        c = bin(sx).count("1") + bin(sy).count("1")
+        A[c] = min(A[c], d)
+        sy -= 1
 print(*A, sep="\n")
