877a611f9e84fed0f00c8e077218e2c756653e52
藤平燎 <fujihiraryo@team-lab.com>
2020-08-08 15:32:01 +0900

    E完成

--- other/MSOL2020/E.py
+++ other/MSOL2020/E.py
@@ -17,16 +17,16 @@ for s in range(1 << n):
                 DX[s][i] = min(DX[s][i], abs(X[i] - X[j]) * P[i])
                 DY[s][i] = min(DY[s][i], abs(Y[i] - Y[j]) * P[i])
 
+BC = [bin(s).count("1") for s in range(1 << n)]
 A = [10**20] * (n + 1)
 for sx in range(1 << n):
     rx = ((1 << n) - 1) ^ sx
     sy = rx
-    while sy >= 0:
-        sy &= rx
+    for _ in range(1 << (n - BC[sx])):
+        sy = (sy - 1) & rx
         d = 0
         for i in range(n):
             d += min(DX[sx][i], DY[sy][i])
-        c = bin(sx).count("1") + bin(sy).count("1")
+        c = BC[sx] + BC[sy]
         A[c] = min(A[c], d)
-        sy -= 1
 print(*A, sep="\n")
