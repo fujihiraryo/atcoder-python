03d46f41151de96708ad61246e125d8338d7828b
fujihiraryo <fujihiraryo@fujihiraryos-Mac-mini.local>
2020-11-23 13:27:28 +0900

    update E.py

--- ABC/ABC184/E.py
+++ ABC/ABC184/E.py
@@ -3,7 +3,7 @@ import collections
 INF = 10 ** 20
 h, w = map(int, input().split())
 A = [input() for i in range(h)]
-D = collections.defaultdict(lambda: set())
+D = collections.defaultdict(lambda: [])
 for i in range(h):
     for j in range(w):
         if A[i][j] == "S":
@@ -11,26 +11,19 @@ for i in range(h):
         if A[i][j] == "G":
             gi, gj = i, j
         if A[i][j].islower():
-            D[A[i][j]].add((i, j))
+            D[A[i][j]].append((i, j))
 C = [[INF for j in range(w)] for i in range(h)]
 C[si][sj] = 0
 Q = collections.deque([(si, sj)])
 while Q:
     i, j = Q.popleft()
-    S = set()
-    for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        ni, nj = i + di, j + dj
+    for ni, nj in [(i, j + 1), (i, j - 1), (i + 1, j), (i - 1, j)] + D[A[i][j]]:
         if not (0 <= ni < h and 0 <= nj < w and A[ni][nj] != "#"):
             continue
-        S.add((ni, nj))
-    if A[i][j].islower():
-        S |= D[A[i][j]]
-        D[A[i][j]].clear()
-    for ni, nj in S:
         if C[i][j] + 1 < C[ni][nj]:
             C[ni][nj] = C[i][j] + 1
             Q.append((ni, nj))
-    S.clear()
+    D[A[i][j]].clear()
 if C[gi][gj] == INF:
     print(-1)
 else:
