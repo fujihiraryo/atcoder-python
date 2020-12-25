881f4d25606c06ac4e27a51f87010b49d12299db
fujihiraryo <fujihiraryo.team-lab.com>
2020-12-26 02:34:30 +0900

    EDDP Longest Path

--- EDDP/G2.py
+++ EDDP/G2.py
@@ -1,31 +1,26 @@
-import copy
-
 n, m = map(int, input().split())
-from_set = [set() for _ in range(n)]
-to_set = [set() for _ in range(n)]
-stack = set(range(n))
+from_list = [[] for _ in range(n)]
+to_list = [[] for _ in range(n)]
 for _ in range(m):
     x, y = map(lambda x: int(x) - 1, input().split())
-    from_set[y].add(x)
-    to_set[x].add(y)
-    if y in stack:
-        stack.remove(y)
-_from_set = copy.deepcopy(from_set)
+    from_list[y].append(x)
+    to_list[x].append(y)
 # トポロジカルソート
+from_set = [set(from_list[x]) for x in range(n)]
+stack = [x for x in range(n) if len(from_list[x]) == 0]
 order = []
 while stack:
     x = stack.pop()
     order.append(x)
-    while to_set[x]:
-        y = to_set[x].pop()
+    while to_list[x]:
+        y = to_list[x].pop()
         from_set[y].remove(x)
         if from_set[y]:
             continue
-        stack.add(y)
+        stack.append(y)
 # 最長パス
-from_set = _from_set
 dp = [0] * n
 for i in order:
-    for j in from_set[i]:
+    for j in from_list[i]:
         dp[i] = max(dp[i], dp[j] + 1)
 print(max(dp))
