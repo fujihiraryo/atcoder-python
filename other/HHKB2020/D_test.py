ec8973b1b9428984df663044a265458f6541eb9b
fujihiraryo <fujihiraryo.team-lab.com>
2021-01-21 02:13:15 +0900

    HHKB2020 D

--- other/HHKB2020/D_test.py
+++ other/HHKB2020/D_test.py
@@ -1,12 +1,11 @@
 MOD = 10 ** 9 + 7
 for _ in range(int(input())):
     n, a, b = map(int, input().split())
-    if a + b > n:
-        print(0)
-        continue
     if a > b:
         a, b = a, b
     total = (n - a + 1) * (n - b + 1)
-    cover = (n - a) * a + n - b + 1 + (b - 2) * (n - a - b + 1)
+    cover = 0
+    for i in range(n - b + 1):
+        cover += min(n - a, i + b - 1) - max(0, i - a + 1) + 1
     ans = pow(total, 2, MOD) - pow(cover, 2, MOD)
     print(ans % MOD)
