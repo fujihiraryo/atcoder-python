bb5e3db859dcd9cc9adb5fb0af5ee5df90db59e1
fujihiraryo <fujihiraryo.team-lab.com>
2021-02-05 01:33:33 +0900

    AC!

--- other/ALPC/K.py
+++ other/ALPC/K.py
@@ -1,22 +1,24 @@
 import sys
 
 input = sys.stdin.readline
+MOD = 998244353
+BIT = 30
+MASK = (1 << BIT) - 1
 
 
 class LazySegmentTree:
-    def __init__(self, n, ide, ope):
-        self.size = n
-        self.data = [ide] * (self.size << 1)
-        for k in range(1, self.size)[::-1]:
-            self.data[k] = self.data[k << 1] + self.data[k << 1 | 1]
-        self.memo = [ope] * (self.size << 1)
+    def __init__(self, a, ide, idf, fold, compose, apply):
+        self.size = len(a)
         self.ide = ide
-        self.ope = ope
-
-    def initialize(self, a):
+        self.idf = idf
+        self.fold = fold
+        self.compose = compose
+        self.apply = apply
+        self.data = [self.ide] * (self.size << 1)
+        self.memo = [self.idf] * (self.size << 1)
         self.data[self.size :] = a
         for k in range(1, self.size)[::-1]:
-            self.data[k] = self.data[k << 1] + self.data[k << 1 | 1]
+            self.data[k] = self.fold(self.data[k << 1], self.data[k << 1 | 1])
 
     def __covering_index(self, i, j):
         i0 = i + self.size
@@ -47,80 +49,88 @@ class LazySegmentTree:
             j0 >>= 1
 
     def __lazy_update(self, k):
-        if self.memo[k] == self.ope:
+        if self.memo[k] == self.idf:
             return
         if k < self.size:
-            self.memo[k << 1] = self.memo[k] * self.memo[k << 1]
-            self.data[k << 1] = self.memo[k](self.data[k << 1])
-            self.memo[k << 1 | 1] = self.memo[k] * self.memo[k << 1 | 1]
-            self.data[k << 1 | 1] = self.memo[k](self.data[k << 1 | 1])
-        self.memo[k] = self.ope
+            self.memo[k << 1] = self.compose(self.memo[k], self.memo[k << 1])
+            self.data[k << 1] = self.apply(self.memo[k], self.data[k << 1])
+            self.memo[k << 1 | 1] = self.compose(self.memo[k], self.memo[k << 1 | 1])
+            self.data[k << 1 | 1] = self.apply(self.memo[k], self.data[k << 1 | 1])
+        self.memo[k] = self.idf
 
-    def range_apply(self, i, j, ope):
-        for k in [*self.__covering_index(i, j)][::-1]:
+    def range_apply(self, i, j, f):
+        covering = [*self.__covering_index(i, j)]
+        for k in covering[::-1]:
             self.__lazy_update(k)
         for k in self.__covered_index(i, j):
-            self.memo[k] = ope * self.memo[k]
-            self.data[k] = ope(self.data[k])
-        for k in self.__covering_index(i, j):
-            left = self.data[k << 1]
-            right = self.data[k << 1 | 1]
-            self.data[k] = left + right
+            self.memo[k] = self.compose(f, self.memo[k])
+            self.data[k] = self.apply(f, self.data[k])
+        for k in covering:
+            self.data[k] = self.fold(self.data[k << 1], self.data[k << 1 | 1])
 
     def range_merge(self, i, j):
-        for k in [*self.__covering_index(i, j)][::-1]:
+        covering = [*self.__covering_index(i, j)]
+        for k in covering[::-1]:
             self.__lazy_update(k)
         x = self.ide
         for k in self.__covered_index(i, j):
-            x = x + self.data[k]
+            x = self.fold(x, self.data[k])
         return x
 
 
-MOD = 998244353
+def encode(x0, x1):
+    return (x0 << BIT) + x1
 
 
-class Monoid:
-    # example(Range Sum)
-    def __init__(self, value=0, length=1):
-        self.value = value
-        self.length = length
-
-    def __add__(self, other):
-        value = self.value + other.value
-        length = self.length + other.length
-        return Monoid(value % MOD, length)
+def decode(x):
+    return x >> BIT, x & MASK
 
 
-class Operator:
-    # example(Range Affine)
-    def __init__(self, param=(1, 0)):
-        self.param = param
-
-    def __call__(self, monoid):
-        b, c = self.param
-        value = b * monoid.value + monoid.length * c
-        return Monoid(value % MOD, monoid.length)
-
-    def __mul__(self, other):
-        b0, c0 = self.param
-        b1, c1 = other.param
-        b, c = b0 * b1, b0 * c1 + c0
-        return Operator((b % MOD, c % MOD))
-
-    def __eq__(self, other):
-        return self.param == other.param
+def fold(x, y):
+    x0, x1 = decode(x)
+    y0, y1 = decode(y)
+    z0 = (x0 + y0) % MOD
+    z1 = x1 + y1
+    return encode(z0, z1)
 
 
-n, q = map(int, input().split())
-(*a,) = map(int, input().split())
-a = [Monoid(a[i], 1) for i in range(n)]
-tree = LazySegmentTree(n, Monoid(), Operator())
-tree.initialize(a)
-for _ in range(q):
-    cmd, *query = map(int, input().split())
-    if cmd == 0:
-        i, j, b, c = query
-        tree.range_apply(i, j, Operator((b, c)))
-    else:
-        i, j = query
-        print(tree.range_merge(i, j).value)
+def compose(f, g):
+    f0, f1 = decode(f)
+    g0, g1 = decode(g)
+    h0 = f0 * g0 % MOD
+    h1 = (f0 * g1 + f1) % MOD
+    return encode(h0, h1)
+
+
+def apply(f, x):
+    f0, f1 = decode(f)
+    x0, x1 = decode(x)
+    y0 = (f0 * x0 + f1 * x1) % MOD
+    y1 = x1
+    return encode(y0, y1)
+
+
+def main():
+    n, q = map(int, input().split())
+    (*a,) = map(int, input().split())
+    tree = LazySegmentTree(
+        a=[encode(a[i], 1) for i in range(n)],
+        ide=encode(0, 1),
+        idf=encode(1, 0),
+        fold=fold,
+        compose=compose,
+        apply=apply,
+    )
+    for _ in range(q):
+        cmd, *query = map(int, input().split())
+        if cmd == 0:
+            i, j, b, c = query
+            tree.range_apply(i, j, encode(b, c))
+        else:
+            i, j = query
+            x, _ = decode(tree.range_merge(i, j))
+            print(x)
+
+
+if __name__ == "__main__":
+    main()
