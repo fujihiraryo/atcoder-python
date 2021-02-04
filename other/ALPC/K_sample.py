af9d9cf2c06db6daf84c82946c6d9b683009ffe6
fujihiraryo <fujihiraryo.team-lab.com>
2021-02-04 23:19:40 +0900

    update sample.py

--- other/ALPC/K_sample.py
+++ other/ALPC/K_sample.py
@@ -8,12 +8,12 @@ print(*a)
 for _ in range(q):
     t = random.randint(0, 2)
     if t == 0:
-        i = random.randint(0, n)
-        j = random.randint(i + 1, n + 1)
+        i = random.randint(0, n - 1)
+        j = random.randint(i + 1, n)
         b = random.randint(1, MOD)
         c = random.randint(0, MOD)
         print(0, i, j, b, c)
     else:
-        i = random.randint(0, n)
-        j = random.randint(i + 1, n + 1)
+        i = random.randint(0, n - 1)
+        j = random.randint(i + 1, n)
         print(1, i, j)
