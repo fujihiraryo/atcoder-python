06ca1f331f84cfede36ae711ff47388de3a6f2b5
fujihiraryo <fujihiraryo.team-lab.com>
2021-02-15 11:54:25 +0900

    AC

--- ARC/ARC112/B.py
+++ ARC/ARC112/B.py
@@ -1,7 +1,10 @@
 b, c = map(int, input().split())
-a = (c - 1) // 2
-if b < -b or a - b < b - c // 2:
-    ans = a + 1 + c // 2 + 1
-else:
-    ans = max(a - b, b) - min(-b, b - c // 2) + 1
+a0 = -b - (c - 1) // 2
+a1 = -b + (c - 1) // 2
+a2 = b - c // 2
+a3 = b + max(0, c - 2) // 2
+ans = abs(a0 - a1) + 1 + abs(a2 - a3) + 1
+x, y = max(a0, a2), min(a1, a3)
+if x <= y:
+    ans -= abs(x - y) + 1
 print(ans)
