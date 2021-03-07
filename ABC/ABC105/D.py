7f10d2663d236844181f019656f791bd4fa5bb18
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-08 02:08:57 +0900

    D - Candy Distribution

---
+++ ABC/ABC105/D.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+n, m = map(int, input().split())
+*a, = map(int, input().split())
+a = [a[i] % m for i in range(n)]
+s = [0]
+for i in range(n):
+    s.append((s[i] + a[i]) % m)
+c = defaultdict(int)
+for x in s:
+    c[x] += 1
+ans = 0
+for x in c:
+    ans += c[x] * (c[x] - 1) // 2
+print(ans)
