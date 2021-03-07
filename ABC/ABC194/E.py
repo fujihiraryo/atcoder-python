2533ee082514e963c2076739263001a4d88cea3f
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-07 12:07:22 +0900

    E

---
+++ ABC/ABC194/E.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+*a, = map(int, input().split())
+idx = [[-1] for x in range(max(a) + 1)]
+for i in range(n):
+    idx[a[i]].append(i)
+for lst in idx:
+    lst.append(n)
+for x, lst in enumerate(idx):
+    k = len(lst)
+    for i in range(k - 1):
+        if lst[i + 1] - lst[i] - 1 >= m:
+            print(x)
+            exit()
+print(x + 1)
