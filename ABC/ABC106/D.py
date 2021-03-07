7d84738b0e76a047dab51b3ec4704562f8da8125
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-08 01:52:50 +0900

    ABC106 D - AtCoder Express 2

--- ABC/ABC106/D.py
+++ ABC/ABC106/D.py
@@ -1,12 +1,17 @@
-s = input()
-k = int(input())
-cnt = 0
-for x in s:
-    if x == "1":
-        cnt += 1
-    else:
-        break
-if k <= cnt:
-    print("1")
-else:
-    print(x)
+n, m, q = map(int, input().split())
+table = [[0] * (n + 2) for _ in range(n + 2)]
+for _ in range(m):
+    li, ri = map(int, input().split())
+    table[1][ri] += 1
+    table[1][n + 1] -= 1
+    table[li + 1][ri] -= 1
+    table[li + 1][n + 1] += 1
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        table[i][j] += table[i - 1][j]
+for j in range(1, n + 1):
+    for i in range(1, n + 1):
+        table[i][j] += table[i][j - 1]
+for _ in range(q):
+    i, j = map(int, input().split())
+    print(table[i][j])
