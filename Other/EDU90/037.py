bc51d0ca83fea2674d173ef919dc22fdd379b3f2
fujihiraryo <fujihiraryo.team-lab.com>
2021-05-12 03:34:35 +0900

    典型90 37日目

---
+++ Other/EDU90/037.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+INF = 1 << 60
+w, n = map(int, input().split())
+l, r, v = [], [], []
+for _ in range(n):
+    li, ri, vi = map(int, input().split())
+    l.append(li)
+    r.append(ri)
+    v.append(vi)
+lst = [-INF] * (w + 1)
+lst[0] = 0
+for i in range(n):
+    new = lst[:]
+    dq = deque()
+    for j in range(l[i], w + 1):
+        while dq and lst[dq[-1]] <= lst[j - l[i]]:
+            dq.pop()
+        dq.append(j - l[i])
+        if j > r[i] and dq[0] == j - r[i] - 1:
+            dq.popleft()
+        new[j] = max(new[j], lst[dq[0]] + v[i])
+    lst = new
+ans = lst[w]
+print(ans if ans > 0 else -1)
