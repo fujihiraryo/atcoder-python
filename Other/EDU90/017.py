06395e7ba4af1cd1e8a9d080970eea1c5920b9fd
fujihiraryo <fujihiraryo.team-lab.com>
2021-05-15 13:55:39 +0900

    典型90 17日目

--- Other/EDU90/017.py
+++ Other/EDU90/017.py
@@ -1,10 +1,34 @@
+class BIT:
+    def __init__(self, n):
+        self.n = n
+        self.a = [0] * (n + 1)
+
+    def sum(self, i):
+        s = 0
+        while i:
+            s += self.a[i]
+            i -= i & -i
+        return s
+
+    def add(self, i, x):
+        i += 1
+        while i <= self.n:
+            self.a[i] += x
+            i += i & -i
+
+
 n, m = map(int, input().split())
 lst = [tuple(map(int, input().split())) for _ in range(m)]
+lst.sort()
+l = [lr[0] for lr in lst]
+r = [lr[1] for lr in lst]
 ans = 0
-for i in range(m - 1):
-    li, ri = lst[i]
-    for j in range(i + 1, m):
-        lj, rj = lst[j]
-        if li < lj < ri < rj or lj < li < rj < ri:
-            ans += 1
+cur = 0
+bit = BIT(n)
+for j in range(m):
+    if l[j] != l[cur]:
+        for i in range(cur, j):
+            bit.add(r[i], 1)
+        cur = j
+    ans += bit.sum(r[j]) - bit.sum(l[j] + 1)
 print(ans)
