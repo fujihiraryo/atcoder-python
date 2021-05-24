9019a1a10f84905220369d68eea870594fe8f913
fujihiraryo <fujihiraryo.team-lab.com>
2021-05-25 03:36:20 +0900

    リファクタリング

--- ABC/ABC194/E.py
+++ ABC/ABC194/E.py
@@ -1,14 +1,12 @@
 n, m = map(int, input().split())
 (*a,) = map(int, input().split())
-idx = [[-1] for x in range(max(a) + 1)]
+idx = [[-1] for x in range(n)]
 for i in range(n):
     idx[a[i]].append(i)
-for lst in idx:
-    lst.append(n)
-for x, lst in enumerate(idx):
-    k = len(lst)
-    for i in range(k - 1):
-        if lst[i + 1] - lst[i] - 1 >= m:
+for x in range(n):
+    idx[x].append(n)
+    for i in range(len(idx[x]) - 1):
+        if idx[x][i + 1] - idx[x][i] > m:
             print(x)
             exit()
-print(x + 1)
+print(n)
