27dc719080e64bee2d57fdd0f6a9f204c7191095
fujihiraryo <fujihiraryo.team-lab.com>
2021-05-25 03:13:26 +0900

    リファクタリング

--- ABC/ABC195/E.py
+++ ABC/ABC195/E.py
@@ -1,14 +1,12 @@
 n = int(input())
-s = [int(i) for i in input()[::-1]]
-x = input()[::-1]
-dp = [[0] * 7 for _ in range(n + 1)]
-for j in range(1, 7):
-    dp[0][j] = 1
-for i in range(1, n + 1):
-    k = s[i - 1] * pow(10, i - 1, 7) % 7
+s = [int(i) for i in input()]
+x = input()
+dp = [["A"] * 7 for _ in range(n + 1)]
+dp[n][0] = "T"
+for i in range(n)[::-1]:
     for j in range(7):
-        if x[i - 1] == "A":
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][(j + k) % 7])
+        if x[i] == dp[i + 1][10 * j % 7]:
+            dp[i][j] = dp[i + 1][10 * j % 7]
         else:
-            dp[i][j] = min(dp[i - 1][j], dp[i - 1][(j + k) % 7])
-print("Aoki" if dp[n][0] else "Takahashi")
+            dp[i][j] = dp[i + 1][(10 * j + s[i]) % 7]
+print("Aoki" if dp[0][0] == "A" else "Takahashi")
--- ABC/ABC196/D.py
+++ ABC/ABC196/D.py
@@ -1,32 +1,22 @@
-from copy import deepcopy
-from itertools import product
+h, w, a, b = map(int, input().split())
 
 
-def solve(grid, a, b):
-    if a == 0 and b == 0:
+def rec(a, b, bit):
+    if bit == 0:
         return 1
-    h, w = len(grid), len(grid[0])
-    for i, j in product(range(h), range(w)):
-        if grid[i][j] == 0:
-            break
+    i = 0
+    tmp = 1
+    while not tmp & bit:
+        i += 1
+        tmp <<= 1
     res = 0
-    if a > 0 and i + 1 < h and grid[i + 1][j] == 0:
-        new_grid = deepcopy(grid)
-        new_grid[i][j] = 1
-        new_grid[i + 1][j] = 1
-        res += solve(new_grid, a - 1, b)
-    if a > 0 and j + 1 < w and grid[i][j + 1] == 0:
-        new_grid = deepcopy(grid)
-        new_grid[i][j] = 1
-        new_grid[i][j + 1] = 1
-        res += solve(new_grid, a - 1, b)
+    if a > 0 and i % w != w - 1 and 1 << (i + 1) & bit:
+        res += rec(a - 1, b, bit - (1 << i) - (1 << i + 1))
+    if a > 0 and i // w != h - 1 and 1 << (i + w) & bit:
+        res += rec(a - 1, b, bit - (1 << i) - (1 << i + w))
     if b > 0:
-        new_grid = deepcopy(grid)
-        new_grid[i][j] = 1
-        res += solve(new_grid, a, b - 1)
+        res += rec(a, b - 1, bit - (1 << i))
     return res
 
 
-h, w, a, b = map(int, input().split())
-grid = [[0] * w for _ in range(h)]
-print(solve(grid, a, b))
+print(rec(a, b, (1 << (h * w)) - 1))
