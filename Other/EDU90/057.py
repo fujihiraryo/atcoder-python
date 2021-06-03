9bb0df2bf592fd9f203eb803a6df5e20602c242b
藤平燎 <fujihiraryo@team-lab.com>
2021-06-03 22:02:20 +0900

    典型90 57日目 Python 107ms

--- Other/EDU90/057.py
+++ Other/EDU90/057.py
@@ -1,11 +1,12 @@
 def free_degree(a, b):
     global MOD
     n, m = len(a), len(a[0])
+    a = [sum(a[i][j] * pow(2, j) for j in range(m)) for i in range(n)]
     r = 0
     for j in range(m):
         p = -1
         for i in range(r, n):
-            if a[i][j]:
+            if a[i] >> j & 1:
                 p = i
                 break
         if p == -1:
@@ -13,14 +14,13 @@ def free_degree(a, b):
         a[p], a[r] = a[r], a[p]
         b[p], b[r] = b[r], b[p]
         for i in range(n):
-            if i == r or not a[i][j]:
+            if i == r or not a[i] >> j & 1:
                 continue
-            for k in range(m):
-                a[i][k] ^= a[r][k]
+            a[i] ^= a[r]
             b[i] ^= b[r]
         r += 1
     for i in range(n):
-        if all(a[i][j] == 0 for j in range(m)) and b[i]:
+        if a[i] == 0 and b[i]:
             return -1
     return m - r
 
