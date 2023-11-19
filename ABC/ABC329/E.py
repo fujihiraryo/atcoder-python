da0d4423c86512e18d2519659aa4bfb55ce1dac5
fujihiraryo <>
2023-11-19 19:04:47 +0900

    ABC329/E

--- ABC/ABC329/E.py
+++ ABC/ABC329/E.py
@@ -1,12 +1,23 @@
 n, m = map(int, input().split())
-s = input()
-t = input()
-lst = []
-for i in range(m):
-    for j in range(m):
-        lst.append(t[: i + 1] + t[j:])
-print(lst)
-for i in range(n):
-    for x in lst:
-        s = s.replace(x, "#" * len(x))
-print(s.split("#"))
+s = list(input())
+t = list(input())
+lst = [i for i in range(n - m + 1)]
+for i in lst:
+    res = True
+    if s[i : i + m] == ["#"] * m:
+        continue
+    for x, y in zip(s[i : i + m], t):
+        if x != "#" and x != y:
+            res = False
+    if res:
+        s[i : i + m] = "#" * m
+        for j in range(max(0, i - (m - 1)), i):
+            if j + m <= n:
+                lst.append(j)
+        for j in range(i + 1, min(i + m, n)):
+            if j + m <= n:
+                lst.append(j)
+if s == ["#"] * n:
+    print("Yes")
+else:
+    print("No")
