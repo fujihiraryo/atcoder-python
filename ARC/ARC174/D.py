c2b29025d5f99cf74fb41fcdbec3a4fdcf34e0af
fujihiraryo <>
2024-03-18 01:20:01 +0900

    ARC174

--- ARC/ARC174/D.py
+++ ARC/ARC174/D.py
@@ -8,7 +8,7 @@ def tle(n):
 
 
 def solve(n):
-    ans = 1
+    ans = 0
     i = 0
     while True:
         x = int("9" * i + "8" + "0" * (i + 1))
@@ -22,21 +22,15 @@ def solve(n):
         y = int("9" * (2 * i))
         if n < x:
             break
-        if x <= n <= y:
-            ans += n - x + 1
-            break
-        ans += y - x + 1
+        ans += min(y, n) - x + 1
         i += 1
-    i = 1
+    i = 0
     while True:
         x = int("1" + "0" * (2 * i))
         y = int("1" + "0" * i + "9" * i)
         if n < x:
             break
-        if x <= n <= y:
-            ans += n - x + 1
-            break
-        ans += y - x + 1
+        ans += min(y, n) - x + 1
         i += 1
     return ans
 
